dd48c412f65cf9fcaa5ab9df4b67b37123a02d47
VissaMoutafis <44686370+VissaMoutafis@users.noreply.github.com>
2026-01-28 10:50:54 +0000

    Deploying to gh-pages from @ VissaMoutafis/VissaMoutafis.github.io@caf8906ac5dd988979841c106dbdf67bec6d5d39 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Courses, Lectures and Supervision",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{
         id: 'social-cv',
         title: 'CV',
